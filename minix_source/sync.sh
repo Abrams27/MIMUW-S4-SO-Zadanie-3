4926a7809ca80ae8bba3ad18fb3ffacd1454146e
Abrams27 <abramowiczmarcin99@gmail.com>
2020-05-07 23:33:06 +0200

    przygotowanie do testow

--- minix_source/sync.sh
+++ minix_source/sync.sh
@@ -4,3 +4,7 @@ cd "$(dirname "$0")" # set current working directory to the directory of the scr
 
 rsync -uav --exclude=".git" ./usr/include/ minix:/usr/include/
 rsync -uav --exclude=".git" ./usr/src/ minix:/usr/src/
+
+scp -r test/change-parent-test.c minix:~/test/change-parent-test.c
+scp -r test/original-parent-test.c minix:~/test/original-parent-test.c
+scp -r test/buildAndRunTests.sh minix:~/test/buildAndRunTests.sh
